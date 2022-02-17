43f321daa22acf6608beaf5e274dcc33b655bc32
minsu12345 <qnchalstn@gmail.com>
2022-02-17 13:00:49 +0900

    20220217
    d

---
+++ Assets/02. Scripts/dom.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello" +
+            "");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
