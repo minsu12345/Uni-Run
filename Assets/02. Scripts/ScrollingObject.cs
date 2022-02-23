c121de9e74ca40a7ec183249ed7cb8800561e111
minsu12345 <qnchalstn@gmail.com>
2022-02-23 12:40:19 +0900

    20220223

---
+++ Assets/02. Scripts/ScrollingObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 10f; //이동속도
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        
+    }
+}
